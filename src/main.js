88c26c3d702116014191061274873cee17aa680a
Dan Taeyoung <provolot@gmail.com>
2020-08-14 18:53:26 -0400

    adding vuex-router-sync

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import { sync } from 'vuex-router-sync'
+import router from '@/router'
+import store from '@/store'
+
 Vue.config.productionTip = false
+
+sync(store, router);
 
 new Vue({
   render: h => h(App),
